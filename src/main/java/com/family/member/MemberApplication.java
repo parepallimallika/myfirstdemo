76469ff8293e5ea3ccfff825a6b8045a3eec3265
parepallimallika <mallika129.parepalli@gmail.com>
2019-11-03 14:26:32 +0530

    change

--- src/main/java/com/family/member/MemberApplication.java
+++ src/main/java/com/family/member/MemberApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MemberApplication {
-
+	//first change
 	public static void main(String[] args) {
 		SpringApplication.run(MemberApplication.class, args);
 	}
